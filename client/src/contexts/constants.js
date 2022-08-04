9dbd9125820be19d42483e3ec61b1d69d125e669
kai to <anhpha90@gmail.com>
2022-08-04 19:47:57 +0700

    prepare

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://secure-tundra-62966.herokuapp.com/api' : 'https://secure-tundra-62966.herokuapp.com/api'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://boiling-peak-41983.herokuapp.com/api' : 'somedeployedURL'
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit mern'
 export const POSTS_LOADED_SUCCESS = 'POSTS_LOADED_SUCCESS'
 export const POSTS_LOADED_FAIL = 'POSTS_LOADED_FAIL'
--- server/index.js
+++ server/index.js
@@ -27,6 +27,12 @@ app.use(express.json());
 app.use(cors())
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
+app.use(
+    cors({
+        origin: "http://localhost:3000", 
+        credentials: true,
+    })
+);
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Sever Started on ${PORT}`))
 
